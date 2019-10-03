d3f945943aa9b09817dbfb54a639a11e62cbba16
Solomon Tayo <solomontayo@yahoo.com>
2019-10-03 13:22:06 +0300

    Added a Python file

---
+++ GIS-Programming-3-11-19.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+
+Это временный скриптовый файл.
+"""
+
+Item_name = "Computer"
+print(Item_name)
+Item_qty = 10 #An Integer
+print(Item_qty)
+Item_value = 1000.23 #A f
+print (Item_value)
+a = input ("Enter the first number: ")
+print (a)
